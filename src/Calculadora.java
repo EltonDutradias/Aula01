2355cde1654bee96d85e38ca55a4ff5bf0548b15
Elton Dutra Dias <elton.dutradias.ti@gmail.com>
2021-06-10 20:18:41 -0300

    implementação de novas operações

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,14 +5,23 @@ public class Calculadora {
 		Scanner leitor = new Scanner (System.in); 
 		double valor1; 
 		double valor2; 
-		double soma;
+		double soma, divisao, multiplicacao, subtracao;
 		System.out.println("Por favor, digite um valor:"); 
 		valor1 = leitor.nextDouble(); 
 		System.out.println("Digite mais um valor:"); 
 		valor2 = leitor.nextDouble(); 
-		soma = valor1 + valor2;
+		soma = valor1 + valor2; 
+		subtracao = valor1 - valor2;  
+		multiplicacao = valor1 * valor2; 
+		divisao = valor1 / valor2; 
+		
 		System.out.println("A soma dos dois valores informados é "+ soma);
-        leitor.close();
+        leitor.close(); 
+		System.out.println("A subtração dos dois valores informados é "+ subtracao); 
+		System.out.println("A multiplicação dos dois valores informados é "+ multiplicacao); 
+		System.out.println("A divisão dos dois valores informados é "+ divisao);
+        leitor.close(); 
+
 	}
 
 }
